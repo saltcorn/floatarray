76133f30337f91cbc070d8747fc9d6dcb0a9a7e5
Tom Nielsen <tanielsen@gmail.com>
2023-01-04 18:49:37 +0000

    stats

--- index.js
+++ index.js
@@ -41,8 +41,33 @@ const floatArray = {
   },
 };
 
+const array_stats = {
+  run: (vs) => {
+    if (!vs || vs.length === 0) return;
+    let max = vs[0],
+      min = vs[0],
+      mini = 0,
+      maxi = 0;
+    for (let i = 1; i < vs.length; i++) {
+      const v = vs[i];
+      if (v > max) {
+        max = v;
+        maxi = i;
+      } else if (v < min) {
+        min = v;
+        mini = i;
+      }
+    }
+    return { max, min, mini, maxi };
+  },
+  isAsync: false,
+  description: "Array min, max, maxi, mini",
+  arguments: [{ name: "query", type: "Object" }],
+};
+
 module.exports = {
   sc_plugin_api_version: 1,
   types: [floatArray],
   plugin_name: "floatarray",
+  functions: { array_stats },
 };
